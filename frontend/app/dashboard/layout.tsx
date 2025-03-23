4c7e50c52e131391a87ced78ff52f9c3e7de05de
Elvin Aghalarov <aghalarovelvin@gmail.com>
2025-03-23 23:33:22 +0400

    frontend: added loading states

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -17,11 +17,13 @@ import React, {
   ForwardRefExoticComponent,
   RefAttributes,
   useEffect,
+  useState,
 } from "react";
 import Loading from "@/components/custom/loading";
 import { User } from "@/interfaces/user";
 import { Bell, Bot, LucideProps } from "lucide-react";
 import { useUserStore } from "@/store/userStore";
+import { usePathname } from "next/navigation";
 
 // This is sample data.
 const data: {
@@ -56,34 +58,46 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   const { user, isLoading, fetchUser } = useUserStore();
+  const [loading, setLoading] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
+
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => setLoading(false), 500);
+
+    return () => clearTimeout(timeout);
+  }, [pathname]);
 
   if (isLoading) return <Loading />;
 
   data.user = user as User;
 
   return (
-    <SidebarProvider>
-      <AppSidebar data={data} />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      {loading && <Loading />}
+      <SidebarProvider>
+        <AppSidebar data={data} />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </>
   );
 }
